dbae52775857c15384cbc58f1c04915a6dc63e78
Josh Driver <keeperofdakeys@gmail.com>
2016-02-28 00:05:34 +1030

    Update psq indentation, clean up code, and fully implement args
    The options --perf, --long and --tree now follow appropriate
    control paths for future modification.

--- src/psq/main.rs
+++ src/psq/main.rs
@@ -5,119 +5,145 @@ extern crate prettytable;
 use prettytable::Table;
 use prettytable::format::FormatBuilder;
 use std::collections::HashMap;
+use std::iter::repeat;
 use procrs::pid::*;
 use procrs::TaskId;
 use argparse::{ArgumentParser, StoreTrue, Store};
 
 fn main() {
-  let opts = parse_args();
-  match opts {
-    ProgOpts{ tree: false, query: q,
-              perf: perf, long: long, .. } => {
-      let mut table = Table::init(
-        PidIter::new_query(q).unwrap()
-          .map(|p_r|
-            p_r.map(|p|
-              row![p.stat.pid, p.stat.ppid, p.stat.comm, p.cmdline.join(" ")]
-            )
-          ).collect::<Result<Vec<_>, String>>()
-          .unwrap()
-      );
-      let format = FormatBuilder::new()
-        .column_separator(' ')
-        .build();
-      table.set_titles(row!["Pid", "Ppid", "Comm", "Cmd"]);
-      table.set_format(format);
-      table.printstd();
-    },
+    let opts = parse_args();
+    let (query, long, perf, verbose, tree) =
+        (opts.query, opts.long, opts.perf, opts.verbose, opts.tree);
 
-    ProgOpts{ tree: true, query: q, .. } => {
-      let proc_map: HashMap<_, _> =
-        PidIter::new().unwrap()
-        .map(|p_r|
-          p_r.map(|p|
-            (p.stat.pid, p)
-          )
-        ).collect::<Result<_, String>>()
-        .unwrap();
+    let mut pids = PidIter::new_query(query).unwrap()
+        .collect::<Result<Vec<_>, _>>().unwrap();
 
-      let mut child_procs = HashMap::new();
-      let mut proc_list = Vec::new();
+    let mut name_indent = HashMap::new();
 
-      for (pid, proc_struct) in &proc_map {
-        proc_list.push(pid);
-        let ppid = proc_struct.stat.ppid;
-        child_procs.entry(ppid)
-          .or_insert(Vec::new())
-          .push(proc_struct);
-      }
-      proc_list.sort();
-      let pid = match q {
-        PidQuery::PidQuery(p) => p,
-        _ => 1
-      };
-      let mut pid_procs = Vec::new();
+    if opts.tree {
+        pids = treeify_names(pids, &mut name_indent);
+    } else {
+        pids.sort_by(|p1, p2| p1.stat.pid.cmp(&p2.stat.pid));
+    };
 
-      let start_procs = match proc_map.get(&pid) {
-        Some(proc_struct) => {
-          pid_procs.push(proc_struct);
-          &pid_procs
-        },
-        None => child_procs.get(&pid).expect("Invalid pid")
-      };
+    let mut table = Table::init(
+        pids.iter().map(|p| {
+            let mut name = match tree {
+                false => String::new(),
+                true => name_indent.remove(&p.stat.pid).unwrap()
+            };
 
-      print_tree(&child_procs, start_procs, "".to_string());
-    }
-  }
+            match (long, perf) {
+                (false, false) => {
+                    name.push_str(&p.stat.comm);
+                    row![p.stat.pid, p.stat.ppid, name]
+                },
+                (true, false) => {
+                    name.push_str(&p.cmdline.join(" "));
+                    row![p.stat.pid, p.stat.ppid, name]
+                },
+                (false, true) => {
+                    name.push_str(&p.stat.comm);
+                    row![p.stat.pid, p.stat.ppid, name]
+                }
+                (true, true) => {
+                    name.push_str(&p.cmdline.join(" "));
+                    row![p.stat.pid, p.stat.ppid, name]
+                }
+            }
+        }).collect::<Vec<_>>()
+    );
+
+    table.set_titles(
+        match (long, perf) {
+            (false, false) =>
+                row!["Pid", "Ppid", "Cmd"],
+            (true, false) =>
+                row!["Pid", "Ppid", "Cmd"],
+            (false, true) =>
+                row!["Pid", "Ppid", "Cmd"],
+            (true, true) =>
+                row!["Pid", "Ppid", "Cmd"]
+        }
+    );
+    table.set_format(
+        FormatBuilder::new()
+            .column_separator(' ')
+            .build()
+    );
+    table.printstd();
 }
 
-fn print_tree(child_procs: &HashMap<TaskId, Vec<&Pid>>,
-              level_procs: &Vec<&Pid>, prefix: String) {
-  let mut proc_list = level_procs.to_vec();
-  proc_list.sort();
-  for proc_struct in proc_list {
-    let pid = &proc_struct.stat.pid;
-
-    println!("{}{}", prefix, proc_struct.stat.comm);
-    let child_list = child_procs.get(pid);
-    if let Some(v) = child_list {
-      print_tree(child_procs, v, format!("{}{}", "  ", prefix));
+// Given a vector of Pid structs, treeify their names, and return them in the right order.
+// This is similar to ps -AH.
+fn treeify_names(pids: Vec<Pid>, name_indents: &mut HashMap<TaskId, String>) -> Vec<Pid> {
+    let mut child_pids = HashMap::new();
+    for pid in pids {
+        let ppid = pid.stat.ppid;
+        child_pids.entry(ppid)
+            .or_insert(Vec::new())
+            .push(pid);
     }
-  }
+    enumerate_children(0, &mut child_pids, name_indents, -1)
+}
+
+// Enumerate children pids, and return them.
+fn enumerate_children(pid: TaskId, child_pids: &mut HashMap<TaskId, Vec<Pid>>,
+    name_indents: &mut HashMap<TaskId, String>, indent: i32) -> Vec<Pid> {
+    name_indents.insert(pid,
+        match indent {
+            i if i >= 0 =>
+                repeat("  ").take(i as usize).collect::<String>(),
+            _ => "".to_owned()
+        }
+    );
+    let mut pids = Vec::new();
+    let ppids = match child_pids.remove(&pid) {
+        Some(v) => v,
+        None => { return pids; }
+    };
+    for pid in ppids {
+        let pid_num = pid.stat.pid;
+        pids.push(pid);
+        pids.append(
+            &mut enumerate_children(pid_num, child_pids, name_indents, indent + 1)
+        );
+    }
+    pids
 }
 
 struct ProgOpts {
-  query: PidQuery,
-  tree: bool,
-  perf: bool,
-  long: bool,
-  verbose: bool
+    query: PidQuery,
+    tree: bool,
+    perf: bool,
+    long: bool,
+    verbose: bool
 }
 
 fn parse_args() -> ProgOpts {
-  let mut opts = ProgOpts {
-    query: PidQuery::NoneQuery,
-    tree: false,
-    perf: false,
-    long: false,
-    verbose: false
-  };
+    let mut opts = ProgOpts {
+        query: PidQuery::NoneQuery,
+        tree: false,
+        perf: false,
+        long: false,
+        verbose: false
+    };
 
-  {
-    let mut ap = ArgumentParser::new();
-    ap.set_description("Query linux processes");
-    ap.refer(&mut opts.tree)
-      .add_option(&["-t", "--tree"], StoreTrue, "Display process tree");
-    ap.refer(&mut opts.perf)
-      .add_option(&["-p", "--perf"], StoreTrue, "Display performance information");
-    ap.refer(&mut opts.long)
-      .add_option(&["-l", "--long"], StoreTrue, "Display more information");
-    ap.refer(&mut opts.verbose)
-      .add_option(&["-v", "--verbose"], StoreTrue, "Verbose output");
-    ap.refer(&mut opts.query)
-      .add_argument("query", Store, "Optional query to search by, pid or string");
-    ap.parse_args_or_exit();
-  }
+    {
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Query linux processes");
+        ap.refer(&mut opts.tree)
+            .add_option(&["-t", "--tree"], StoreTrue, "Display process tree");
+        ap.refer(&mut opts.perf)
+            .add_option(&["-p", "--perf"], StoreTrue, "Display performance information");
+        ap.refer(&mut opts.long)
+            .add_option(&["-l", "--long"], StoreTrue, "Display more information");
+        ap.refer(&mut opts.verbose)
+            .add_option(&["-v", "--verbose"], StoreTrue, "Verbose output");
+        ap.refer(&mut opts.query)
+            .add_argument("query", Store, "Optional query to search by, pid or string");
+        ap.parse_args_or_exit();
+    }
 
-  opts
+    opts
 }
