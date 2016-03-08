93cba309ca52d308012ee01a7856e408209bb817
Josh Driver <keeperofdakeys@gmail.com>
2016-03-08 20:42:14 +0950

    Update documentation

--- implementors/core/raw/trait.Repr.js
+++ implementors/core/raw/trait.Repr.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['procrs'] = [];
+implementors['libc'] = [];implementors['procrs'] = [];implementors['procrs'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
