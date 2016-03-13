95241153b55f3cc4833d25ade86e99aa501fa082
Josh Driver <keeperofdakeys@gmail.com>
2016-03-13 23:47:07 +0950

    Update documentation

--- implementors/core/hash/trait.Hasher.js
+++ implementors/core/hash/trait.Hasher.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['procrs'] = [];implementors['procrs'] = [];implementors['procrs'] = [];
+implementors['libc'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
