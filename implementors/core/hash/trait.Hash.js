2b31582a84297f214f8a48c392035c874ad82d82
Josh Driver <keeperofdakeys@gmail.com>
2016-03-14 21:13:43 +0950

    Update documentation

--- implementors/core/hash/trait.Hash.js
+++ implementors/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['argparse'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='argparse/parser/struct.Var.html' title='argparse::parser::Var'>Var</a>",];implementors['libc'] = [];
+implementors['libc'] = [];implementors['argparse'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='argparse/parser/struct.Var.html' title='argparse::parser::Var'>Var</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
