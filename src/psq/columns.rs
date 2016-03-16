ec7569f39f398e4015ba0ca213ed4d1e2124ff9b
Josh Driver <keeperofdakeys@gmail.com>
2016-03-16 22:10:21 +1030

    Add samples of PidCols

--- src/psq/columns.rs
+++ src/psq/columns.rs
@@ -1,11 +1,25 @@
+use std::str::FromStr;
+
 // FIXME: This may be better in procps
-enum PidColumns {
-    // Columns go here
+enum PidCols {
+    Pid,
+    Ppid
 }
 
 // Implement FromStr to allow parsing a list of columns specified by a user
-// impl FromStr for PidColumns {
+impl FromStr for PidCols {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "pid" => PidCols::Pid,
+            "ppid" => PidCols::Ppid,
+            _ => return Err(()),
+        })
+    }
+}
 
 
 // Implement ToStr to get a list of columns specified by a user
 // impl ToStr for PidColumns {
+// }
