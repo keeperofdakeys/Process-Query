84921d2b7b85907d9c8cdf8618b825c811a9a65e
Josh <keeperofdakeys@gmail.com>
2016-04-03 15:12:58 +1000

    Update documentation

--- implementors/core/hash/trait.Hash.js
+++ implementors/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['argparse'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='argparse/parser/struct.Var.html' title='argparse::parser::Var'>Var</a>",];
+implementors['argparse'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='struct' href='argparse/parser/struct.Var.html' title='argparse::parser::Var'>Var</a>",];implementors['libc'] = [];implementors['procrs'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='enum' href='procrs/pid/enum.PidFile.html' title='procrs::pid::PidFile'>PidFile</a>",];implementors['procrs'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='enum' href='procrs/pid/enum.PidFile.html' title='procrs::pid::PidFile'>PidFile</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
